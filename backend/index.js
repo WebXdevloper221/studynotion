cbc7a2ae13e9b9af1e216ef1c85fe502a21c152e
WebXdevloper221 <rajatshrivastav004@gmail.com>
2026-05-18 23:28:41 +0530

    Update index.js
    fixed update

--- backend/index.js
+++ backend/index.js
@@ -53,34 +53,34 @@
 
 
 const buffer = require("buffer");
- 
+
 if (!buffer.SlowBuffer) {
   buffer.SlowBuffer = buffer.Buffer;
 }
- 
+
 if (typeof global.SlowBuffer === "undefined") {
   global.SlowBuffer = buffer.SlowBuffer;
 }
- 
+
 const express = require("express");
 const app = express();
- 
+
 const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
- 
+
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
- 
+
 dotenv.config();
- 
+
 const PORT = process.env.PORT || 5000;
- 
+
 // Database connect
 async function startApp() {
   try {
@@ -92,39 +92,30 @@ async function startApp() {
     );
     process.exit(1);
   }
- 
+
   // Middlewares
   app.use(express.json());
   app.use(cookieParser());
- 
-  app.use(
-    cors({
-      origin: [
-        "https://studynotion-alpha-five.vercel.app",
-        "http://localhost:3000",
-      ],
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
- 
+
+  // CORS FIX
+  app.use(cors());
+
   app.use(
     fileUpload({
       useTempFiles: true,
       tempFileDir: "/tmp",
     })
   );
- 
+
   // Cloudinary connection
   cloudinaryConnect();
- 
+
   // Routes
   app.use("/api/v1/auth", userRoutes);
   app.use("/api/v1/profile", profileRoutes);
   app.use("/api/v1/course", courseRoutes);
   app.use("/api/v1/payment", paymentRoutes);
- 
+
   // Health route
   app.get(["/", "/api/v1"], (req, res) => {
     return res.json({
@@ -133,7 +124,7 @@ async function startApp() {
     });
   });
 }
- 
+
 startApp().then(() => {
   app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
